feecaec97322f588faec6631a4e09d335073df76
Okap <gangtianyixi007@gmail.com>
2026-08-28 11:25:27 +0900

    微修正

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -26,9 +26,9 @@ class MicropostsController < ApplicationController
 
   def pin
     @micropost = current_user.microposts.find_by(id: params[:id])
-    if @micropost.any?
+    if @micropost.present?
       @pin_microposts = current_user.microposts.pinning
-      if @pin_microposts.any?
+      if @pin_microposts.present?
         @pin_microposts.first.update(pinned: false)
       end
       @micropost.update(pinned: true)
@@ -38,7 +38,7 @@ class MicropostsController < ApplicationController
 
   def unpin
     @micropost = current_user.microposts.find_by(id: params[:id])
-    if @micropost.any?
+    if @micropost.present?
       @micropost.update(pinned: false)
     end
     redirect_to request.referrer || root_url, status: :see_other
